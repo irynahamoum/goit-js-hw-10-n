08277938120d98b2658f00f2a149026e263ae4f3
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2024-01-25 23:32:33 +0100

    find elements

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+const dateInput = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysDisplay = document.querySelector('[data-days]');
+const hoursDisplay = document.querySelector('[data-hours]');
+const minutesDisplay = document.querySelector('[data-minutes]');
+const secondsDisplay = document.querySelector('[data-seconds]');
+
+let userSelectedDate;
 
 function convertMs(ms) {
   const second = 1000;
@@ -33,6 +32,14 @@ console.log(convertMs(2000));
 console.log(convertMs(140000));
 console.log(convertMs(24140000));
 
-const extentions = { enableTime: true, dateFormat: 'Y-m-d H:i' };
+const extentions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
 
-flatpickr('input', extentions);
+flatpickr('input#datetime-picker', extentions);
